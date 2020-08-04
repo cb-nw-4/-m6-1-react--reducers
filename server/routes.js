4b8eca55531f8fa5f5ee61d1b0e78b942af15a9c
Scott Morin <scottanthonymorin@gmail.com>
2020-08-04 14:48:07 -0400

    fix server

--- server/routes.js
+++ server/routes.js
@@ -1,82 +1,28 @@
-/**
-  Endpoints related to feeds (ordered sets of tweets)
-*/
-const router = require('express').Router();
-const { delay } = require('./helpers');
+const router = require("express").Router();
+const { delay } = require("./helpers");
 
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
 
-let state;
-
-router.get('/api/seat-availability', (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
+// Code that is generating the seats.
+// ----------------------------------
+const seats = {};
+const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+for (let r = 0; r < row.length; r++) {
+  for (let s = 1; s < 13; s++) {
+    seats[`${row[r]}-${s}`] = {
+      price: 225,
+      isBooked: false,
     };
   }
-
-  return res.json({
-    bookedSeats: state.bookedSeats,
-    numOfRows: NUM_OF_ROWS,
-    seatsPerRow: SEATS_PER_ROW,
-  });
-});
-
-let lastBookingAttemptSucceeded = false;
-
-router.post('/api/book-seat', async (req, res) => {
-  const { seatId, creditCard, expiration } = req.body;
-
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
-
-  await delay(Math.random() * 3000);
-
-  if (!creditCard || !expiration) {
-    return res.status(400).json({
-      message: 'Please provide credit card information!',
-    });
-  }
-
-  if (isAlreadyBooked) {
-    return res.status(400).json({
-      message: 'This seat has already been booked!',
-    });
-  }
-
-  if (lastBookingAttemptSucceeded) {
-    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-    return res.status(500).json({
-      message: 'An unknown error has occurred. Please try your request again.',
-    });
-  }
-
-  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-  state.bookedSeats[seatId] = true;
-
-  return res.json({
-    success: true,
-  });
-});
-
+}
+// ----------------------------------
 //////// HELPERS
-const getRowName = rowIndex => {
+const getRowName = (rowIndex) => {
   return String.fromCharCode(65 + rowIndex);
 };
 
-// const getRowIndex = rowName => {
-//   return rowName.charCodeAt(0) - 65;
-// };
-
-const randomlyBookSeats = num => {
+const randomlyBookSeats = (num) => {
   const bookedSeats = {};
 
   while (num > 0) {
@@ -92,5 +38,69 @@ const randomlyBookSeats = num => {
 
   return bookedSeats;
 };
+
+let state;
+
+router.get("/api/seat-availability", async (req, res) => {
+  if (!state) {
+    state = {
+      bookedSeats: randomlyBookSeats(30),
+    };
+  }
+
+  await delay(Math.random() * 3000);
+
+  return res.json({
+    seats: seats,
+    bookedSeats: state.bookedSeats,
+    numOfRows: 8,
+    seatsPerRow: 12,
+  });
+});
+
+let lastBookingAttemptSucceeded = false;
+
+router.post("/api/book-seat", async (req, res) => {
+  const { seatId, creditCard, expiration } = req.body;
+
+  if (!state) {
+    state = {
+      bookedSeats: randomlyBookSeats(30),
+    };
+  }
+
+  await delay(Math.random() * 3000);
+
+  const isAlreadyBooked = !!state.bookedSeats[seatId];
+  if (isAlreadyBooked) {
+    return res.status(400).json({
+      message: "This seat has already been booked!",
+    });
+  }
+
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: "Please provide credit card information!",
+    });
+  }
+
+  if (lastBookingAttemptSucceeded) {
+    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+    return res.status(500).json({
+      message: "An unknown error has occurred. Please try your request again.",
+    });
+  }
+
+  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+  state.bookedSeats[seatId] = true;
+
+  return res.status(200).json({
+    status: 200,
+    success: true,
+  });
+});
 
 module.exports = router;

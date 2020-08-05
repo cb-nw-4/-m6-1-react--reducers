b6be6b71391f6d66c5ed9dbd7481d0a9722358b0
Scott Morin <scottanthonymorin@gmail.com>
2020-08-05 12:44:29 -0400

    update/fix server

--- server/routes.js
+++ server/routes.js
@@ -4,55 +4,26 @@ const { delay } = require("./helpers");
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
 
-// Code that is generating the seats.
+// Code that is generating the seats/pricing/availability.
 // ----------------------------------
 const seats = {};
+const pricingOptions = [500, 500, 400, 350, 250, 250, 175, 125];
 const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
 for (let r = 0; r < row.length; r++) {
   for (let s = 1; s < 13; s++) {
     seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
+      price: pricingOptions[r],
+      isBooked: Math.floor(Math.random() * 2) > 0,
     };
   }
 }
 // ----------------------------------
-//////// HELPERS
-const getRowName = (rowIndex) => {
-  return String.fromCharCode(65 + rowIndex);
-};
-
-const randomlyBookSeats = (num) => {
-  const bookedSeats = {};
-
-  while (num > 0) {
-    const row = Math.floor(Math.random() * NUM_OF_ROWS);
-    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
-
-    const seatId = `${getRowName(row)}-${seat + 1}`;
-
-    bookedSeats[seatId] = true;
-
-    num--;
-  }
-
-  return bookedSeats;
-};
-
-let state;
 
 router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
   await delay(Math.random() * 3000);
 
   return res.json({
     seats: seats,
-    bookedSeats: state.bookedSeats,
     numOfRows: 8,
     seatsPerRow: 12,
   });
@@ -63,16 +34,9 @@ let lastBookingAttemptSucceeded = false;
 router.post("/api/book-seat", async (req, res) => {
   const { seatId, creditCard, expiration } = req.body;
 
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
   await delay(Math.random() * 3000);
 
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
-  if (isAlreadyBooked) {
+  if (seats[seatId].isBooked) {
     return res.status(400).json({
       message: "This seat has already been booked!",
     });
@@ -95,7 +59,7 @@ router.post("/api/book-seat", async (req, res) => {
 
   lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
 
-  state.bookedSeats[seatId] = true;
+  seats[seatId].isBooked = true;
 
   return res.status(200).json({
     status: 200,

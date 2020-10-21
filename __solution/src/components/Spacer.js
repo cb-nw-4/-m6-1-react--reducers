3944fc3f06f51cdfc14ce121d472ef05db8d1f4c
Scott Morin <scottanthonymorin@gmail.com>
2020-10-21 16:51:47 -0400

    add lecture notes

--- __solution/src/components/Spacer.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  width: ${p => p.size}px;
-  height: ${p => p.size}px;
-`;
--- __solution/src/utils.js
+++
@@ -1,7 +0,0 @@
-export function range(end) {
-  var ans = [];
-  for (let i = 0; i < end; i++) {
-    ans.push(i);
-  }
-  return ans;
-}

87a7f2fdcd7437bfa3384e8dd76d36950110951e
chris shino <chris.shino@hotmail.com>
2021-01-26 20:19:05 -0500

    done # 2

---
+++ src/components/SeatContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from "react";
+export const SeatContext = createContext();
+
+const initialState = {
+  hasLoaded: false,
+  seats: null,
+  numOfRows: 0,
+  seatsPerRow: 0,
+};
+
+const reducer = () => {};
+
+export const SeatProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const receiveSeatInfoFromServer = (data) => {
+    dispatch({
+      type: "receive-seat-info-from-server",
+      ...data,
+    });
+  };
+
+  return (
+    <SeatContext.Provider
+      value={{ state, actions: { receiveSeatInfoFromServer } }}
+    >
+      {children}
+    </SeatContext.Provider>
+  );
+};

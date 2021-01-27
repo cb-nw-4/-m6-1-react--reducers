f8cc26ad06c6ac05958e168822ac93b06a2d083e
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-01-26 21:46:29 -0500

    ex1,2,3

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { SeatContext } from './SeatContext';
 import GlobalStyles from './GlobalStyles';
 
-function App() {
+const App=()=>{
+  const {
+    state: { numOfRows },
+    actions: { receiveSeatInfoFromServer },
+  } = React.useContext(SeatContext);
+
+  React.useEffect(()=>{
+    fetch('/api/seat-availability')
+    .then(res=>res.json())
+    .then(data=>receiveSeatInfoFromServer(data))
+  },[])
+
   return (
     <>
       <GlobalStyles />
       TODO: write code
+      {numOfRows}
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/SeatContext.js
@@ -0,0 +1,58 @@
+import React, { useReducer } from 'react';
+
+export const SeatContext=React.createContext(null);
+
+const initialState = {
+    hasLoaded: false,
+    seats: null,
+    numOfRows: 0,
+    seatsPerRow: 0,
+};
+
+function reducer(state, action) {
+    console.log("action", action);
+    console.log("state", state);
+// TODO
+    switch (action.type) {
+        case 'receive-seat-info-from-server': {
+            return {
+                ...state,
+                hasLoaded: true,
+                seats: action.seats,
+                numOfRows: action.numOfRows,
+                seatsPerRow: action.seatsPerRow,
+            };
+        }
+        
+        default:
+            throw new Error(`Unrecognized action: ${action.type}`);
+    }
+}
+
+const SeatProvider=({children})=>{
+    const [state, dispatch]=React.useReducer(reducer, initialState);
+
+    const receiveSeatInfoFromServer = (data) => {
+        dispatch({
+            type: "receive-seat-info-from-server",
+            ...data,
+        });
+    };
+    
+
+    return(
+        <SeatContext.Provider
+            value={{
+                state,
+                actions: {
+                    receiveSeatInfoFromServer,
+                },
+            }}
+        >
+            {children}
+        </SeatContext.Provider>
+    )
+
+};
+
+export default SeatProvider;

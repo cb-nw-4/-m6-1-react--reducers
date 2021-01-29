209a12f6aa5a1f652b2135f05f8bd639a6f8918f
Lara Tunc <laratunc@gmail.com>
2021-01-29 15:09:06 -0500

    Reducers workshop complete

---
+++ src/components/PurchaseModal.js
@@ -0,0 +1,195 @@
+import { Dialog, TextField } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import React from 'react';
+import styled from 'styled-components';
+import CloseIcon from '@material-ui/icons/Close';
+
+import { BookingContext } from './BookingContext';
+import { SeatContext } from './SeatContext';
+
+const PurchaseModal = ()=> {
+    const {
+        state: { status, error, selectedSeatId, price },
+        actions: { CancelBookingProcess, PurchaseTicketRequest, PurchaseTicketFailure, PurchaseTicketSuccess }
+    } = React.useContext(BookingContext);
+
+    const { 
+        state : { seats },
+        actions : { MarkSeatAsPurchased }
+    } = React.useContext(SeatContext);
+
+    const seatId = selectedSeatId;
+    const seatRow = selectedSeatId.slice(0,1);
+    const seatNo = selectedSeatId.slice(2,3);
+
+    const [creditCard, setCreditCard] = React.useState("");
+    const [expiration, setExpiration] = React.useState("");
+
+    const handleClick = (ev)=>{
+        ev.preventDefault();
+        PurchaseTicketRequest();
+        
+        const postObject = {seatId:seatId,creditCard:creditCard,expiration:expiration};
+
+        fetch("/api/book-seat", {
+            method: "POST",
+            body: JSON.stringify(postObject),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                if(json.status!==200) {
+                    PurchaseTicketFailure(json);
+                } else {
+                    PurchaseTicketSuccess();
+                    MarkSeatAsPurchased({seats, seatId, price});
+                };
+            })
+
+    };
+
+    return (
+        <Dialog open={selectedSeatId !== null} >
+            <CloseButton onClick={CancelBookingProcess}>
+                <CloseIcon/>
+            </CloseButton>
+            <ModalBox>
+                <Title>Purchase Ticket</Title>
+                <Info>You're purchasing <strong>1</strong> ticket for the price of ${price}</Info>
+                <Table>
+                    <Row>
+                        <TableHeader>Row</TableHeader>
+                        <TableHeader>Seat</TableHeader>
+                        <TableHeader>Price</TableHeader>
+                    </Row>
+                    <Row>
+                        <TableData>{seatRow}</TableData>
+                        <TableData>{seatNo}</TableData>
+                        <TableData>${price}</TableData>
+                    </Row>
+                </Table>
+                <GrayArea>
+                    <FormTitle>Enter payment details</FormTitle> 
+                    <Form>
+                        <TextField  
+                            label="Credit card" 
+                            variant="outlined"
+                            style={{marginRight:"10px", flex:"2"}}
+                            value={creditCard}
+                            onChange={(ev)=>{setCreditCard(ev.target.value)}}
+                        />
+                        <TextField  
+                            label="Expiration date" 
+                            variant="outlined"
+                            style={{marginRight:"10px", flex:"1"}}
+                            value={expiration}
+                            onChange={(ev)=>{setExpiration(ev.target.value)}}
+                        />
+                        <Button
+                            onClick={handleClick}
+                        >{
+                            status==="awaiting-response" 
+                            ? <CircularProgress size="30px" style={{color:"white"}}/> 
+                            :`PURCHASE`
+                        }</Button>
+                    </Form>
+                    {error ? <Error>{error}</Error> : <Error/>}
+                </GrayArea>
+            </ModalBox>
+        </Dialog>
+    );
+};
+
+const CloseButton = styled.button`
+outline:none;
+border:none; 
+display:flex;
+justify-content:flex-end;
+background-color:white;
+`;
+
+const ModalBox = styled.div`
+min-width:45%; 
+background-color:white;
+color:black;
+z-index:5;
+padding: 10px 0px;
+`;
+
+const Title = styled.div`
+font-size: 24px;
+font-weight:bolder; 
+padding: 10px 40px;
+`;
+
+const Info = styled.div`
+font-size: 18px;
+padding: 10px 40px;
+`;
+
+const Table = styled.div`
+font-size: 18px;
+display: flex;
+flex-direction:column;
+padding: 10px 40px;
+`; 
+
+const Row = styled.div`
+padding: 15px 5px; 
+display: flex; 
+border-bottom: 1px solid #ccd0db; 
+`; 
+
+const TableHeader = styled.div`
+flex:1; 
+font-weight:bolder; 
+display:flex;
+`;
+
+const TableData = styled.div`
+flex:1;
+display:flex;
+`;
+
+const GrayArea = styled.div`
+width:100%; 
+font-size: 18px;
+background-color: #f3f3f3; 
+margin: 20px 0px;
+padding: 20px 40px;
+`;
+
+const Form = styled.form`
+display:flex;
+font-size: 18px;
+`;
+
+const FormTitle = styled.div`
+padding: 10px 0px;
+`;
+
+const Button = styled.button`
+border: 1px solid #4f56a4;
+background-color:#4f56a4;
+color:white;
+padding: 10px;
+border-radius:10px;
+flex:0.5;
+&:active {
+    transform: scale(0.8);
+}
+&:focus {
+    outline:none;
+};
+`;
+
+const Error = styled.div`
+padding: 10px 0px;
+color:red;
+`;
+
+
+export default PurchaseModal; 
--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -1,64 +1,131 @@
 import React from 'react';
 import styled from 'styled-components';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { SeatContext } from './SeatContext';
+import { BookingContext } from './BookingContext';
+import seatAvailable from '../assets/seat-available.svg'
+import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
+import PurchaseModal from './PurchaseModal';
 
 import { getRowName, getSeatNum } from '../helpers';
 import { range } from '../utils';
+import PurchaseConfirmation from './PurchaseConfirmation';
 
 const TicketWidget = () => {
-  // TODO: use values from Context
-  const numOfRows = 6;
-  const seatsPerRow = 6;
+  const {
+    state: { numOfRows, seatsPerRow, hasLoaded, seats },
+  } = React.useContext(SeatContext);
 
-  // TODO: implement the loading spinner <CircularProgress />
-  // with the hasLoaded flag
+  const {
+    state: { status, selectedSeatId },
+    actions: { BeginBookingProcess }
+  } = React.useContext(BookingContext);
+
 
   return (
     <Wrapper>
-      {range(numOfRows).map(rowIndex => {
-        const rowName = getRowName(rowIndex);
+    {!hasLoaded 
+    ? 
+      <SpinnerWrapper>
+        <CircularProgress />
+      </SpinnerWrapper> 
+    : 
+      <MainWrapper>
+        {selectedSeatId && <PurchaseModal/>}
+        {range(numOfRows).map(rowIndex => {
+          const rowName = getRowName(rowIndex);
 
-        return (
-          <Row key={rowIndex}>
-            <RowLabel>Row {rowName}</RowLabel>
-            {range(seatsPerRow).map(seatIndex => {
-              const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
+          return (
+            <Row key={rowIndex}>
+              <RowLabel>Row {rowName}</RowLabel>
+              {range(seatsPerRow).map(seatIndex => {
+                const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
+                const seat = seats[seatId];
+                const seatPrice = seats[seatId]["price"];
 
-              return (
-                <SeatWrapper key={seatId}>
-                  {/* TODO: Render the actual <Seat /> */}
-                </SeatWrapper>
-              );
-            })}
-          </Row>
-        );
-      })}
+                return (
+                  <SeatWrapper key={seatId}>
+                    {
+                      <Tippy content={`Row ${rowName}, Seat ${seatIndex+1} - $${seatPrice}`}>
+                        <Seat 
+                          disabled={seat["isBooked"]}
+                          onClick={()=>{BeginBookingProcess({seatId:seatId,seatPrice:seatPrice})}}
+                        >
+                          <SeatImg 
+                            alt={seatIndex} 
+                            src={seatAvailable} 
+                            width={36}
+                            height={36}
+                            greyedOut={seat["isBooked"] ? "grayscale(100%)" : ""}
+                          />
+                        </Seat>
+                      </Tippy>
+                    }
+                  </SeatWrapper>
+                );
+              })}
+            </Row>
+          );
+        })}
+      </MainWrapper>
+    }
+    {status==="purchased" && <PurchaseConfirmation/>}
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  background: #eee;
-  border: 1px solid #ccc;
+  height:100vh; 
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+`;
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:center;
   border-radius: 3px;
   padding: 8px;
 `;
 
 const Row = styled.div`
   display: flex;
-  position: relative;
-
-  &:not(:last-of-type) {
-    border-bottom: 1px solid #ddd;
-  }
+  align-items: center;
 `;
 
 const RowLabel = styled.div`
   font-weight: bold;
+  padding-right: 10px;
+  width:60px;
 `;
 
 const SeatWrapper = styled.div`
   padding: 5px;
+  background: #eee;
+`;
+
+const SpinnerWrapper = styled.div`
+width:100%;
+height:100vh; 
+display:flex;
+align-items: center;
+justify-content:center;
+`;
+
+const Seat = styled.button`
+border:none; 
+outline:none; 
+padding: 2px;
+&:active {
+  box-shadow: 0px 0px 5px 3px #a1c4f4;
+}
+`;
+
+const SeatImg = styled.img`
+filter: ${({greyedOut})=> greyedOut}
 `;
 
 export default TicketWidget;

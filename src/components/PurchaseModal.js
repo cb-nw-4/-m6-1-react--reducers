69321ea851aacf5ec0fe1d6c7e2479eadb750f6e
Vanessa Chan <chw.vanessa@gmail.com>
2021-02-02 22:11:50 -0500

    Ex-8AB

--- src/components/PurchaseModal.js
+++ src/components/PurchaseModal.js
@@ -8,55 +8,99 @@ import TextField from '@material-ui/core/TextField';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import {BookingContext} from "./BookingContext";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const PurchaseModal = () =>{
-    const { 
-        state: { selectedSeatId },
-        actions: {clearSelection},
-      } = useContext(BookingContext);
+  const [creditCard, setCreditCard] = React.useState("");
+  const [expiration, setExpiration] = React.useState("");
 
-    useEffect(()=>{
-        const handleKeyDown = (event)=>{
-            console.log('some key presses');
-            if(event.key==='Enter'){
-                clearSelection();
-                console.log("enter pressed")
-            }
-        }
-        window.addEventListener("keydown", handleKeyDown);
-        
-        return ()=> {
-            window.removeEventListener("keydown", handleKeyDown)};
-    },[]);
+  const updateCreditCart = (ev)=>{
+    setCreditCard(ev.target.value);
+  };
+
+  const updateExpiration = (ev)=>{
+    setExpiration(ev.target.value);
+  };
+
+  const { 
+    state: { selectedSeatId,price },
+    actions: {clearSelection},
+  } = useContext(BookingContext);
+
+  const contentStyle = {color: 'black'};
+  const tableStyle = {color: 'black'};
+  const tableContainerStyle = {margin: 'auto'};
+  const paymentStyle = {
+    width: '100%'
+   };
+  const inputsStyle={ margin: '15px 15px' };
+  const inputStyling={
+     marginRight:'10px'
+    };
+  const buttonStyle={
+    color: 'white', 
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    padding: "16px 15px"};
 
     return (
     <>
-    <Dialog open={selectedSeatId !== null} >
-    <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+    <Dialog open={selectedSeatId !== null} onClose={()=>clearSelection()}>
+    <DialogTitle id="form-dialog-title">Purchase ticket</DialogTitle>
     <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
+          <DialogContentText style={contentStyle}>
+            You are purchasing 1 ticket for the price of ${price}.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
     </DialogContent>
-    <DialogActions>
-          <Button  color="primary">
-            Cancel
+    <DialogContent style={tableContainerStyle}>
+    <Table  style={tableStyle}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Row</TableCell>
+                <TableCell>Seat</TableCell>
+                <TableCell>Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>{selectedSeatId?selectedSeatId.charAt(0):null}</TableCell>
+                <TableCell>{selectedSeatId?selectedSeatId.substring(2,4):null}</TableCell>
+                <TableCell>${price}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+    </DialogContent>
+    <DialogTitle style={paymentStyle}>
+        Enter payment details
+      </DialogTitle>
+    <DialogActions style={inputsStyle}>
+      <form>
+    <TextField
+            id="credit" style={inputStyling} required
+            placeholder="Credit Card"
+            type="number" variant="outlined"
+            onChange={(ev)=>updateCreditCart(ev)}
+            value={creditCard}
+          />
+          <TextField
+            id="exp" style={inputStyling} required
+            placeholder="Expiration"
+            type="number" variant="outlined"
+            onChange={(ev)=>updateExpiration(ev)}
+            value={expiration}
+          />
+          <Button 
+          style={buttonStyle} 
+          type="submit" 
+          onClick={(ev)=>ev.preventDefault()}>
+            PURCHASE
           </Button>
-          <Button  color="primary">
-            Subscribe
-          </Button>
+          </form>
         </DialogActions>
-
-
     </Dialog>
     </>
     );
